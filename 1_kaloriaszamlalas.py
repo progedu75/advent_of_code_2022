d3538ba6e22bb474c33c4baffd876f1c0556c7fe
ProgEdu <progedu75@gmail.com>
2022-12-01 21:41:35 +0100

    init

--- 1_kaloriaszamlalas.py
+++ 1_kaloriaszamlalas.py
@@ -1,18 +1,29 @@
+adatbazis = []
 adatok = []
 elf = []
+
 with open('files/01_calories.txt', 'r', encoding='utf-8') as forrasfajl:
     for sor in forrasfajl:
         adat_1 = sor.strip().split()
-    print(adat_1)
-    '''
-    adat = forrasfajl.readline().strip()
-    print(adat)
-    if adat != '' and adat != '\n':
-        elf.append(adat)
-    else:
-        adatok.append(elf)
-        elf = []
-print(elf)
-'''
+        adatok.append(adat_1)
+    # print(adatok)
+    hossz = len(adatok)
+
+    forrasfajl.seek(0)
+    for i in range(1, len(adatok)):
+        adat_2 = forrasfajl.readline().strip()
+        if adat_2 != '' and adat_2 != '\n':
+            elf.append(int(adat_2))
+        else:
+            adatbazis.append(elf)
+            elf = []
+print(f'{adatbazis=}')
+osszegek = []
+for lista in adatbazis:
+    osszegek.append(sum(lista))
+print(f'{osszegek=}')
+print(f'max = {max(osszegek)}')
+
+
 
 
